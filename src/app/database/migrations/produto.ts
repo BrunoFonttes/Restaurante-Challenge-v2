d622d2fa63e363fe96e104bd13a75c1dbf85854c
Bruno <brunobfonttes@gmail.com>
2020-11-20 18:30:08 -0300

    Altera a migration de produto retirando unique de nome

--- src/app/database/migrations/produto.ts
+++ src/app/database/migrations/produto.ts
@@ -6,9 +6,9 @@ export const produto: MigrationData = {
 CREATE TABLE IF NOT EXISTS produto (
 	id UUID DEFAULT gen_random_uuid(),
 	id_restaurante UUID,
-  nome text UNIQUE NOT NULL,
-  preco decimal(12,2) NOT NULL,
-  foto bytea NOT NULL,
+  	nome text  NOT NULL,
+  	preco decimal(12,2) NOT NULL,
+  	foto bytea NOT NULL,
 	categoria text NOT NULL,
 	FOREIGN KEY (id_restaurante) REFERENCES restaurante(id) ON DELETE CASCADE,
 	PRIMARY KEY(nome, id_restaurante)
